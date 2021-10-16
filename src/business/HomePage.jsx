18511271adf00f4e8231bd4654d45c243a578161
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-10-16 18:28:19 -0300

    feat: add sintax enforcer

--- src/business/HomePage.jsx
+++ src/business/HomePage.jsx
@@ -1,24 +1,21 @@
-import { useMemo } from "react";
+import React, { useMemo } from 'react';
 import { differenceInYears } from 'date-fns';
-import { useDispatch, useSelector } from "react-redux";
-import { Edit, DeleteOutline } from "@material-ui/icons";
+import { useDispatch, useSelector } from 'react-redux';
+import { Edit, DeleteOutline } from '@material-ui/icons';
 
 import {
   actions as routeActions,
   types as routes,
-} from "../reducers/routes.actions";
-
+} from '../reducers/routes.actions';
 
 const HomePage = () => {
   const dispatch = useDispatch();
   const { loading, data } = useSelector((state) => state.home);
 
-  const orderedData = useMemo(() => {
-    return data.sort((a, b) => {
-      const today = new Date();
-      return differenceInYears(today, a.dataNascimento) - differenceInYears(today, b.dataNascimento);
-    })
-  }, [data]);
+  const orderedData = useMemo(() => data.sort((a, b) => {
+    const today = new Date();
+    return differenceInYears(today, b.dataNascimento) - differenceInYears(today, a.dataNascimento);
+  }), [data]);
 
   if (loading) {
     return <div>Carregando usuários</div>;
@@ -39,20 +36,26 @@ const HomePage = () => {
 
         <tbody>
           {orderedData.map((u) => {
+            const age = differenceInYears(new Date(), u.dataNascimento);
             return (
               <tr key={u.id}>
                 <td>{u.nome}</td>
                 <td>
-                  {u.cidade}/{u.uf}
+                  {u.cidade}
+                  /
+                  {u.uf}
                 </td>
-                <td>{differenceInYears(new Date(), u.dataNascimento)}</td>
+                <td>
+                  {age}
+                  {' '}
+                  ano
+                  {age > 1 ? 's' : ''}
+                </td>
                 <td>
                   <Edit
-                    onClick={() =>
-                      dispatch(
-                        routeActions.redirectTo(routes.USER, { id: u.id })
-                      )
-                    }
+                    onClick={() => dispatch(
+                      routeActions.redirectTo(routes.USER, { id: u.id }),
+                    )}
                   />
                   <DeleteOutline />
                 </td>
